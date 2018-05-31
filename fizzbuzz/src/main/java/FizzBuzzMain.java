375a7d87d6f11de6d8120b64bd367329db48c70c
Yi Han <hanystudy@gmail.com>
2018-05-31 22:45:21 +0800

    [hy] add runnable entrypoint and task

---
+++ fizzbuzz/src/main/java/FizzBuzzMain.java
@@ -0,0 +1,10 @@
+import name.hanyi.fizzbuzz.Printer;
+
+public class FizzBuzzMain {
+
+    static Printer printer = new Printer();
+
+    public static void main(String[] args) {
+        printer.print(1, 100);
+    }
+}
