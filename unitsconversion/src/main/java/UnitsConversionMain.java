752e0ce7b677989d6ccb3ffaaef16a32057c5968
Yi Han <hanystudy@gmail.com>
2018-06-01 23:45:56 +0800

    [hy] add main class

---
+++ unitsconversion/src/main/java/UnitsConversionMain.java
@@ -0,0 +1,10 @@
+import name.hanyi.unitsconversion.UnitsConversion;
+
+public class UnitsConversionMain {
+
+    static UnitsConversion unitsConversion = new UnitsConversion();
+
+    public static void main(String[] args) {
+        System.out.println(unitsConversion.get(1D));
+    }
+}
