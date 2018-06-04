26311655a370b3b9784be67c8f300e6e7c88ebb6
Yi Han <hanystudy@gmail.com>
2018-06-04 10:10:01 +0800

    [hy] quickly support inch to cm

--- unitsconversion/src/main/java/name/hanyi/unitsconversion/UnitsConversion.java
+++ unitsconversion/src/main/java/name/hanyi/unitsconversion/UnitsConversion.java
@@ -6,9 +6,24 @@ import java.math.RoundingMode;
 public class UnitsConversion {
     public static final int PRECISION = 8;
     public static final double RATIO_POUND_TO_KILO = 0.45359237D;
+    public static final double RATIO_INCH_TO_CM = 2.54D;
+    private String sourceUnit;
+    private String targetUnit;
 
     public BigDecimal get(double number) {
-        return BigDecimal.valueOf(number * RATIO_POUND_TO_KILO)
+        return BigDecimal.valueOf(calculate(number))
                 .setScale(PRECISION, RoundingMode.HALF_UP);
+    }
+
+    private double calculate(double number) {
+        if (this.sourceUnit.equals("inch") && this.targetUnit.equals("cm")) {
+            return number * RATIO_INCH_TO_CM;
+        }
+        return number * RATIO_POUND_TO_KILO;
+    }
+
+    public void setUnits(String sourceUnit, String targetUnit) {
+        this.sourceUnit = sourceUnit;
+        this.targetUnit = targetUnit;
     }
 }
--- unitsconversion/src/test/java/name/hanyi/unitsconversion/UnitsConversionTest.java
+++ unitsconversion/src/test/java/name/hanyi/unitsconversion/UnitsConversionTest.java
@@ -17,8 +17,15 @@ public class UnitsConversionTest {
 
     @Test
     public void getKilogramsByGivenPounds() {
+        unitsConversion.setUnits("pound", "kilogram");
         assertThat(unitsConversion.get(1).doubleValue(), is(0.45359237D));
         assertThat(unitsConversion.get(1.5D).doubleValue(), is(0.68038856D));
         assertThat(unitsConversion.get(100D).doubleValue(), is(45.359237D));
+    }
+
+    @Test
+    public void getCentimetersByGivenInches() {
+        unitsConversion.setUnits("inch", "cm");
+        assertThat(unitsConversion.get(1).doubleValue(), is(2.54D));
     }
 }
