6a6367b860f7e582a419b7549a4e3c5d6fe1c33f
Yi Han <hanystudy@gmail.com>
2018-06-10 21:50:16 +0800

    [hy] fix main

--- unitsconversion/src/main/java/UnitsConversionMain.java
+++ unitsconversion/src/main/java/UnitsConversionMain.java
@@ -5,6 +5,7 @@ public class UnitsConversionMain {
     static UnitsConversion unitsConversion = new UnitsConversion();
 
     public static void main(String[] args) {
+        unitsConversion.setUnits("cm", "inch");
         System.out.println(unitsConversion.get(1D));
     }
 }
